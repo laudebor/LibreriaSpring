ff47c556ddc5f0dcabece9e5d3f4fc971af34a0c
Laura de Borbón <laudebor@gmail.com>
2022-03-30 22:03:08 -0300

    commit 3

---
+++ src/main/java/com/example/libreria/servicios/AutorServicio.java
@@ -0,0 +1,71 @@
+package com.example.libreria.servicios;
+
+import com.example.libreria.entidades.Autor;
+import com.example.libreria.errores.ErrorServicio;
+import com.example.libreria.repositorios.AutorRepositorio;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorServicio {
+    
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+    
+    public void cargar(String nombre) throws ErrorServicio{
+        validar(nombre);
+        
+        Autor autor = new Autor();
+        autor.setNombre(nombre);
+        autor.setAlta(Boolean.TRUE);
+        autorRepositorio.save(autor);
+    }
+    
+    public void modificar(String id, String nombre) throws ErrorServicio{
+        
+        validar(nombre);
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Autor autor = respuesta.get();
+            autor.setNombre(nombre);
+            autorRepositorio.save(autor);
+        }else{
+            throw new ErrorServicio("El autor indicado no se encuentra en la base de datos");
+        }
+    }
+    
+    public void darBaja(String id) throws ErrorServicio{
+        
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Autor autor = respuesta.get();
+            autor.setAlta(Boolean.FALSE);
+            autorRepositorio.save(autor);
+        }else{
+            throw new ErrorServicio("El autor indicado no se encuentra en la base de datos");
+        }
+    }
+    
+    public void darAlta(String id) throws ErrorServicio{
+        
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Autor autor = respuesta.get();
+            autor.setAlta(Boolean.TRUE);
+            autorRepositorio.save(autor);
+        }else{
+            throw new ErrorServicio("El autor indicado no se encuentra en la base de datos");
+        }
+    }
+    
+    public void validar(String nombre) throws ErrorServicio{
+        if(nombre==null||nombre.isEmpty()){
+            throw new ErrorServicio("Debe indicar un nombre");
+        }
+    }
+    
+}
---
+++ src/main/java/com/example/libreria/servicios/EditorialServicio.java
@@ -0,0 +1,74 @@
+package com.example.libreria.servicios;
+
+import com.example.libreria.entidades.Editorial;
+import com.example.libreria.errores.ErrorServicio;
+import com.example.libreria.repositorios.EditorialRepositorio;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EditorialServicio {
+    
+    @Autowired 
+    private EditorialRepositorio editorialRepositorio;
+    
+    public void cargar(String nombre) throws ErrorServicio{
+        
+        validar(nombre);
+        
+        Editorial editorial = new Editorial();
+        editorial.setNombre(nombre);
+        editorial.setAlta(Boolean.TRUE);
+        editorialRepositorio.save(editorial);
+        
+    }
+    
+    public void modificar(String id, String nombre) throws ErrorServicio{
+        
+        validar(nombre);
+        
+        Optional<Editorial> respuesta = editorialRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Editorial editorial = respuesta.get();
+            editorial.setNombre(nombre);
+            editorialRepositorio.save(editorial);
+        }else{
+            throw new ErrorServicio("La editorial indicada no se encuentra en la base de datos");
+        }
+    }
+    
+    public void darAlta(String id) throws ErrorServicio{
+        
+        Optional<Editorial> respuesta = editorialRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Editorial editorial = respuesta.get();
+            editorial.setAlta(Boolean.TRUE);
+            editorialRepositorio.save(editorial);
+        }else{
+            throw new ErrorServicio("La editorial indicada no se encuentra en la base de datos");
+        }
+    }
+    
+    public void darBaja(String id) throws ErrorServicio{
+        
+        Optional<Editorial> respuesta = editorialRepositorio.findById(id);
+        
+        if(respuesta.isPresent()){
+            Editorial editorial = respuesta.get();
+            editorial.setAlta(Boolean.FALSE);
+            editorialRepositorio.save(editorial);
+        }else{
+            throw new ErrorServicio("La editorial indicada no se encuentra en la base de datos");
+        }
+    }
+    
+    public void validar(String nombre) throws ErrorServicio{
+        if(nombre==null||nombre.isEmpty()){
+            throw new ErrorServicio("Debe indicar un nombre");
+        }
+    }
+    
+}
