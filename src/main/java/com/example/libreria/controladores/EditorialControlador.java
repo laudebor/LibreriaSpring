69197ebd0f1bd6748de751c3ee3beb79e07d85f5
Laura de Borbón <52927859+laudebor@users.noreply.github.com>
2022-05-03 20:39:39 -0300

    commit 14

--- src/main/java/com/example/libreria/controladores/EditorialControlador.java
+++ src/main/java/com/example/libreria/controladores/EditorialControlador.java
@@ -1,8 +1,17 @@
 package com.example.libreria.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//@RequestMapping("/editorial")
 public class EditorialControlador {
+    
+//    @GetMapping("/registroEditorial")
+//    public String registroEditorial(){
+//        return "registroEditorial";
+//    }
     
 }
